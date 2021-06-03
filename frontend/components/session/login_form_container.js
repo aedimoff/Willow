42e001cfebe3dde66cb5abb42dd53e99ed9e1a16
Arianne Dimoff <ariannedimoff@gmail.com>
2021-06-03 11:44:12 -0700

    fix errors bug, code cleanup

--- frontend/components/session/login_form_container.js
+++ frontend/components/session/login_form_container.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { login } from '../../actions/session_actions';
-import { Link } from 'react-router-dom';
 import SessionForm from './session_form';
 import { openModal, closeModal } from '../../actions/modal_actions';
 
-const mapStateToProps = ({ errors }) => ({
-    errors: errors.session,
+const mapStateToProps = state => ({
+    errors: state.errors,
     formType: 'Log In',
     // navLink: <Link to="/signup">Create an Account</Link>,
 });
--- frontend/components/session/signup_form_container.js
+++ frontend/components/session/signup_form_container.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { signup } from '../../actions/session_actions';
 import SessionForm from './session_form';
 import { openModal, closeModal } from '../../actions/modal_actions';
 
 
-const mapStateToProps = ({ errors }) => ({
-    errors: errors.session,
+const mapStateToProps = state => ({
+    errors: state.errors,
     formType: 'Sign Up',
     // navLink: <Link to="/login">Log In!</Link>,
 });

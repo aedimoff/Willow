4e2f492dbe24756fc85bd616d32f0e9fa88bd58f
Arianne Dimoff <ariannedimoff@gmail.com>
2021-06-05 16:28:58 -0700

    refactor navbar style

--- frontend/components/navbar/nav_bar.jsx
+++ frontend/components/navbar/nav_bar.jsx
@@ -6,14 +6,14 @@ const NavBar = ({ currentUser, logout, openModal }) => {
 
     const loggedOut = () => (
         <nav className="login-signup">
-            <button onClick={() => openModal('Log In')}>Log In</button>
+            <button className="toggle-login-logout" onClick={() => openModal('Log In')}>Log In</button>
         </nav>
     );
 
     const loggedIn = () => (
-        <nav className="nav-greeting">
-            <h2 >Welcome {currentUser.email}</h2>
-            <button onClick={logout}>Log Out</button>
+        <nav className="login-status">
+            <h2 >Welcome, {currentUser.email}!</h2>
+            <button className="toggle-login-logout" onClick={logout}>Log Out</button>
         </nav>  
     )
 
@@ -21,7 +21,7 @@ const NavBar = ({ currentUser, logout, openModal }) => {
         <header className="nav-bar">
             <div className="place-holder"></div>
             <h1 className="logo">Willow</h1>
-            <div>
+            <div className="nav-menu">
                 {currentUser ? loggedIn() : loggedOut()}
             </div>
         </header>

bcc101e4b3f3cdc28bd007e61f50fdcd6d6bfdfc
Arianne Dimoff <ariannedimoff@gmail.com>
2021-06-09 08:59:42 -0700

    add styling to css

--- frontend/components/navbar/nav_bar.jsx
+++ frontend/components/navbar/nav_bar.jsx
@@ -7,20 +7,24 @@ const NavBar = ({ currentUser, logout, openModal }) => {
 
     const loggedOut = () => (
         <nav className="login-signup">
-            <button className="button toggle-login-logout" onClick={() => openModal('Log In')}>Log In</button>
+            <button className="secondary-button" onClick={() => openModal('Log In')}>Log In</button>
         </nav>
     );
 
     const loggedIn = () => (
         <nav className="login-status">
-            <h2 >Welcome, {currentUser.email}!</h2>
-            <button className="button toggle-login-logout" onClick={logout}>Log Out</button>
+            <h2 className="nav-bar-greeting">Welcome, {currentUser.email}!</h2>
+            <button className="secondary-button" onClick={logout}>Log Out</button>
         </nav>  
     )
 
     return (
         <header className="nav-bar">
-            <div className="place-holder"></div>
+            <div className="nav-bar-options">
+                <p className="nav-bar-link">Buy</p>
+                <p className="nav-bar-link">Rent</p>
+                <p className="nav-bar-link">Sell</p>
+            </div>
             <div className="logo">
                 <BsHouseDoorFill className="willow-icon"/>
                 <h1 className="logo-title">Willow</h1>

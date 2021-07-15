674fcc7d05ae8da0e9a358181da1171e96ad6b40
Arianne Dimoff <ariannedimoff@gmail.com>
2021-07-14 21:37:32 -0700

    style listing create form

--- frontend/components/listing/listing_create.jsx
+++ frontend/components/listing/listing_create.jsx
@@ -1,115 +1,158 @@
-import React from 'react';
-import { BsHouseDoorFill } from 'react-icons/bs';
+import React from "react";
+import { BsHouseDoorFill } from "react-icons/bs";
 
 class ListingCreate extends React.Component {
-    constructor(props) {
-        super(props)
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.handleFile = this.handleFile.bind(this)
-        this.state = this.props.listing
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFile = this.handleFile.bind(this);
+    this.state = this.props.listing;
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("listing[address]", this.state.address);
+    formData.append("listing[description]", this.state.description);
+    formData.append("listing[zipcode]", this.state.zipcode);
+    formData.append("listing[city]", this.state.city);
+    formData.append("listing[state]", this.state.state);
+    formData.append("listing[price]", this.state.price);
+    formData.append("listing[status]", this.state.status);
+    formData.append("listing[property_type]", this.state.property_type);
+    formData.append("listing[beds]", this.state.beds);
+    formData.append("listing[baths]", this.state.baths);
+    formData.append("listing[lat]", this.state.lat);
+    formData.append("listing[lng]", this.state.lng);
+
+    const { images } = this.state;
+    for (let i = 0; i < images.length; i++) {
+      formData.append("listing[images][]", images[i]);
     }
 
-    handleSubmit(e) {
-        e.preventDefault()
-        const formData = new FormData();
-        formData.append('listing[address]', this.state.address);
-        formData.append('listing[description]', this.state.description);
-        formData.append('listing[zipcode]', this.state.zipcode);
-        formData.append('listing[city]', this.state.city);
-        formData.append('listing[state]', this.state.state);
-        formData.append('listing[price]', this.state.price);
-        formData.append('listing[status]', this.state.status);
-        formData.append('listing[property_type]', this.state.property_type);
-        formData.append('listing[beds]', this.state.beds);
-        formData.append('listing[baths]', this.state.baths);
-        formData.append('listing[lat]', this.state.lat);
-        formData.append('listing[lng]', this.state.lng);
+    this.props.createListing(formData);
+  }
 
-        const { images } = this.state;
-        for (let i = 0; i < images.length; i++) {
-            formData.append("listing[images][]", images[i]);
-        }
+  handleFile(e) {
+    this.setState({ images: e.currentTarget.files });
+  }
 
-        this.props.createListing(formData)
-    }
+  update(field) {
+    return (e) => this.setState({ [field]: e.currentTarget.value });
+  }
 
-    handleFile(e) {
-        this.setState({images: e.currentTarget.files })
-    }
-
-    update(field) {
-        return e => this.setState({[field]: e.currentTarget.value})
-    }
-
-    render() {
-        return(
-            <div className="listing-create-container">
-                <form className="listing-create-form" onSubmit={this.handleSubmit}>
-                    <div className="willow-header" >
-                        <BsHouseDoorFill className="willow-icon"/>
-                        <h1 className="header">Willow</h1>
-                    </div>
-                    <label className="input">Street Address
-                        <input className="create-listing-input" type="text" 
-                        value={this.state.address}
-                        onChange={this.update('address')}/>
-                    </label>
-                    <label className="input">City
-                        <input className="create-listing-input" type="text" 
-                        value={this.state.city}
-                        onChange={this.update('city')}/>
-                    </label>
-                    <label className="input">State
-                        <input className="create-listing-input" type="text" 
-                        value={this.state.state}
-                        onChange={this.update('state')}/>
-                    </label>
-                    <label className="input">Zipcode
-                        <input className="create-listing-input" type="text" 
-                        value={this.state.zipcode}
-                        onChange={this.update('zipcode')}/>
-                    </label>
-                    <label className="input">Number of Beds
-                        <input className="create-listing-input" type="text" 
-                        value={this.state.beds}
-                        onChange={this.update('beds')}/>
-                    </label>
-                    <label className="input">Number of Baths
-                        <input className="create-listing-input" type="text" 
-                        value={this.state.baths}
-                        onChange={this.update('baths')}/>
-                    </label>
-                    <label className="input">Description
-                        <textarea className="create-listing-input"
-                        value={this.state.description}
-                        onChange={this.update('description')}
-                        />
-                    </label>
-                    <label className="input">Listing Price
-                        <input className="create-listing-input" type="text" 
-                        value={this.state.price}
-                        onChange={this.update('price')}/>
-                    </label>
-                    <label className="input">Property Type
-                        <select className="create-listing-input" value={this.state.property_type}
-                            onChange={this.update('property_type')}>
-                            <option defaultValue="Select"/>
-                            <option value="house">House</option>
-                            <option value="apartment">Apartment</option>
-                            <option value="lot">Lot</option>
-                        </select>
-                    </label>
-                    <div className="upload-button-container">
-                        <input className="upload-button" type="file" 
-                            onChange={e => this.setState({ images: e.target.files })}
-                            multiple
-                        />
-                    </div>
-                    <button className="secondary-button" id="create-listing-button" type="submit">Create Listing</button>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="listing-create-container">
+        <BsHouseDoorFill className="header" id="listing-logo-header" />
+        <h3 className="listing-header">Sell Your Home on Willow!</h3>
+        <form className="listing-create-form" onSubmit={this.handleSubmit}>
+          {/* <div className="willow-header">
+            <BsHouseDoorFill className="willow-icon" />
+          </div> */}
+          <label>
+            <input
+              className="create-listing-input"
+              type="text"
+              value={this.state.address}
+              onChange={this.update("address")}
+              placeholder="Street Address"
+            />
+          </label>
+          <label>
+            <input
+              className="create-listing-input"
+              type="text"
+              value={this.state.city}
+              onChange={this.update("city")}
+              placeholder="City"
+            />
+          </label>
+          <label>
+            <input
+              className="create-listing-input"
+              type="text"
+              value={this.state.state}
+              onChange={this.update("state")}
+              placeholder="State"
+            />
+          </label>
+          <label>
+            <input
+              className="create-listing-input"
+              type="text"
+              value={this.state.zipcode}
+              onChange={this.update("zipcode")}
+              placeholder="Zipcode"
+            />
+          </label>
+          <label>
+            <input
+              className="create-listing-input"
+              type="text"
+              value={this.state.beds}
+              onChange={this.update("beds")}
+              placeholder="Number of Beds"
+            />
+          </label>
+          <label>
+            <input
+              className="create-listing-input"
+              type="text"
+              value={this.state.baths}
+              onChange={this.update("baths")}
+              placeholder="Number of Baths"
+            />
+          </label>
+          <label>
+            <textarea
+              className="create-listing-input"
+              value={this.state.description}
+              onChange={this.update("description")}
+              placeholder="Description"
+            />
+          </label>
+          <label>
+            <input
+              className="create-listing-input"
+              type="text"
+              value={this.state.price}
+              onChange={this.update("price")}
+              placeholder="Listing Price"
+            />
+          </label>
+          <label>
+            <select
+              className="create-listing-input"
+              value={this.state.property_type}
+              onChange={this.update("property_type")}
+              placeholder="Property Type"
+            >
+              <option placeholder="Select" />
+              <option value="house">House</option>
+              <option value="apartment">Apartment</option>
+              <option value="lot">Lot</option>
+            </select>
+          </label>
+          <div className="upload-button-container">
+            <input
+              className="upload-button"
+              type="file"
+              onChange={(e) => this.setState({ images: e.target.files })}
+              multiple
+            />
+          </div>
+          <button
+            className="button"
+            id="create-listing-button"
+            type="submit"
+          >
+            Create Listing
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default ListingCreate;

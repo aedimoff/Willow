94704229906ff5213964c87113bcb19931bf73b1
Arianne Dimoff <ariannedimoff@gmail.com>
2021-07-20 14:43:31 -0700

    remove migration file

--- db/migrate/20210714183320_rename_saver_id.rb
+++
@@ -1,5 +0,0 @@
-class RenameSaverId < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :saves, :user_id, :user_id
-  end
-end
--- db/migrate/20210720213537_change_user_id.rb
+++
@@ -1,4 +0,0 @@
-class ChangeUserId < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
